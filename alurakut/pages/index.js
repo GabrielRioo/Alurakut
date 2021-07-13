bc4678da646f0e49a213186a1f4475f8aaaf8e1f
GabrielRioo <gabrielpaivario@gmail.com>
2021-07-12 21:11:29 -0300

    Projeto Inicial

---
+++ alurakut/pages/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
+const Box = styled.div `
+  background: #fff;
+  border-radius: 8px;
+`; 
+
+export default function Home() {
+  return (
+    <main>
+      <Box>Imagem</Box>
+      <Box>Bem Vindos</Box>
+      <Box>Amigos</Box>
+    </main>
+  ) 
+  
+}
