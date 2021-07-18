3e5d6239a0b602c05662dc62c6d119437973e601
GabrielRioo <gabrielpaivario@gmail.com>
2021-07-17 23:32:32 -0300

    inicializando com as apis

---
+++ pages/api/comunidades.js
@@ -0,0 +1,7 @@
+export default async function recebedorDeRequest(req, res) {
+  const TOKEN = "e39a0a9a791675dee4e07e57ac8157";
+  console.log(TOKEN);
+  response.json({
+    dados: "Algum dado qualquer",
+  });
+}
