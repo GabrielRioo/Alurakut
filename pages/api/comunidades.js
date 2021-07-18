11858e705f4eeabca40e9f17be964407245ba063
GabrielRioo <gabrielpaivario@gmail.com>
2021-07-18 17:28:43 -0300

    usando a api para adicionar novas comunidades

--- pages/api/comunidades.js
+++ pages/api/comunidades.js
@@ -1,7 +1,21 @@
+import { SiteClient } from "datocms-client";
+
 export default async function recebedorDeRequest(req, res) {
-  const TOKEN = "e39a0a9a791675dee4e07e57ac8157";
-  console.log(TOKEN);
-  response.json({
-    dados: "Algum dado qualquer",
-  });
+  if (req.method === "POST") {
+    const TOKEN = "e39a0a9a791675dee4e07e57ac8157";
+    const client = new SiteClient(TOKEN);
+
+    const registroCriado = await client.items.create({
+      itemType: "976217", // ID do model de comunidade criado pelo Dato.
+      title: "Comunidade de Teste",
+      imageUrl: "https://github.com/GabrielRioo.png",
+      creatorSlug: "GabrielRioo",
+    });
+
+    console.log(TOKEN);
+    res.json({
+      dados: "Algum dado qualquer",
+      registroCriado: registroCriado,
+    });
+  }
 }
